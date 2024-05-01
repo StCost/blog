8b598be08e7780d64a9ac9a2e5e1b9964d60733c
StCost <kostya010511@gmail.com>
2024-05-01 19:01:57 +0300

    improve collapse machine article

--- components/Youtube.tsx
+++ components/Youtube.tsx
@@ -4,8 +4,12 @@ const Youtube = (props) => {
   return (
     <div className="aspect-w-16 aspect-h-9">
       <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/7H-hzkZlbzY?si=2rD0oSgIsFfsAvNO"
         title="YouTube video player"
-        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
         {...props}
       />
