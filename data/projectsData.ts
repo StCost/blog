9d97a8a55cd6f03889b937ab28338a5a671542ff
StCost <kostya010511@gmail.com>
2024-06-12 11:28:23 +0300

    Edit projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,9 @@
 const projectsData = [
   {
-    title: "TRP: Text Role Play",
-    description: `Text engine for role playing games such as DnD. It allows to create and play text adventures with friends. It is a web application with a simple interface and a lot of features. Including inventory, perks, and character stats. Based on Fallout PnP ruleset. For limited testing purposes: Login/Password: phktybmvrmgoswhcgz@tmmcv.net`,
-    imgSrc: "/static/images/projects/time-machine.jpg",
-    href: "https://stcost.github.io/text-roleplay"
+    title: "COLLAPSE MACHINE",
+    description: `My dream co-op open-world FPS offers the ultimate adventure in an infinite procedural immersive world. Traverse the sands in a mobile home vehicles, exploring secrets and treasures with friends (or foes), all while experiencing the thrill of discovery without constraints`,
+    imgSrc: "/static/images/projects/library_header.jpeg",
+    href: "https://store.steampowered.com/app/2980830/COLLAPSE_MACHINE/"
   },
   {
     title: "RISING BONEvR",
@@ -12,16 +12,19 @@ const projectsData = [
     href: "https://store.steampowered.com/app/1775350/RISING_BONEvR/"
   },
   {
+    title: "Portfolio Web Projects",
+    description: `List of short articles of commercial web projects I worked on and what experience I gained from them.`,
+    href: "/tags/portfolio"
+  },
+  {
     title: "Lifeinvader",
     description: `Simple social network for GTA 5 role play community server. Users, posts, comments, likes, and more. LibertyRP server is long time dead, so website is not available anymore.`,
-    imgSrc: "/static/images/projects/Lifeinvader-GTAV.webp",
     href: "https://github.com/StCost/lifeinvader"
   },
   {
-    title: "[to be announced]",
-    description: `First person Co-op adventure shooter. Driving a vehicle around procedural endless wasteland of forgotten planet. Destructive voxel terrain, physics based vehicles, and a lot of enemies to shoot from most surprising places you can find in locations, built by old world civilization.`,
-    imgSrc: "/static/images/projects/under.png",
-    href: "https://store.steampowered.com/search/?developer=Dreaming%20Saints"
+    title: "TRP: Text Role Play",
+    description: `Text engine for role playing games such as DnD. It allows to create and play text adventures with friends. It is a web application with a simple interface and a lot of features. Including inventory, perks, and character stats. Based on Fallout PnP ruleset. For limited testing purposes: Login/Password: phktybmvrmgoswhcgz@tmmcv.net`,
+    href: "https://stcost.github.io/text-roleplay"
   }
 ];
 
