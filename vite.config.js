4dc8078c54d8913ebdc5025b4c9f12124f0809fb
StCost <kostya010511@gmail.com>
2025-07-18 04:46:03 +0300

    Add rollup-plugin-visualizer for bundle analysis and update Vite configuration
    - Introduced a new script `build:analyze` in package.json for generating bundle analysis.
    - Configured Vite to use rollup-plugin-visualizer for visualizing the build output.
    - Updated package-lock.json to include new dependencies related to the visualizer.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react({
-    jsxRuntime: 'automatic'
-  })],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic'
+    }),
+    visualizer({
+      template: 'treemap', // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'dist/bundle-analysis.html',
+    })
+  ],
   base: '/blog/',
   server: {
     port: 3000,
