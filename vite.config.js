6214de27a145231ded508e174a9e1ad80bdc8ec7
StCost <kostya010511@gmail.com>
2025-07-23 05:13:00 +0300

    Remove unused dependencies and scripts from package.json and package-lock.json to streamline project setup

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,10 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     react({
       jsxRuntime: "automatic",
-    }),
-    visualizer({
-      template: "treemap", // or sunburst
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-      filename: "dist/bundle-analysis.html",
     }),
   ],
   base: "/blog/",
